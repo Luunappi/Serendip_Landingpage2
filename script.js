dea94d863647442c5f86323a206cae50ae464223
Daniel Koch <danielkoch@Daniels-MacBook-Air.local>
2023-11-03 19:05:37 +0200

    version 1.0

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const icon = document.querySelector(".icons")
 const topBar = document.querySelector(".top-bar")
+const iconsTopBar = document.querySelector(".icons-top-bar")
 const observer = new IntersectionObserver( 
-  ([e]) => topBar.classList.toggle("scrolled", e.intersectionRatio < 1),
+  ([e]) => 
+    {
+    topBar.classList.toggle("scrolled", e.intersectionRatio < 1)
+  
+    iconsTopBar.classList.toggle("scrolled", e.intersectionRatio < 1)},
   { 
     rootMargin: '-36px 0px 0px 0px',
     threshold: [1]
