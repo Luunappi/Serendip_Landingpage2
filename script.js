1bd241c5a1aa43d3eb8fdeaf3b7a033b0fd19496
Daniel Koch <danielkoch@Daniels-MacBook-Air.local>
2023-11-03 16:40:34 +0200

    Fix most of the site, still WIP

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-window.addEventListener('scroll', function() {
-  var topBar = document.querySelector('.top-bar');
-  var pageYOffsetValue = window.innerWidth < 768 ? 150 : 680; // Change the value here for mobile devices
-  if (window.scrollY > pageYOffsetValue) { 
-    topBar.classList.add('scrolled');
-  } else {
-    topBar.classList.remove('scrolled');
-  }
-});
+const icon = document.querySelector(".icons")
+const topBar = document.querySelector(".top-bar")
+const observer = new IntersectionObserver( 
+  ([e]) => topBar.classList.toggle("scrolled", e.intersectionRatio < 1),
+  { 
+    rootMargin: '-36px 0px 0px 0px',
+    threshold: [1]
+   }
+);
+
+observer.observe(icon);
